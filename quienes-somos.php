8272f6798eb8c6601bab31a0e3770dd3f7b41f41
Pablo Lires <pablolires@gmail.com>
2021-08-17 16:31:16 -0300

    Agregado de pagina quienes somos

---
+++ quienes-somos.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Bienvenido al Método OnTarget, queremos presentarte a todo nuestro equipo, el que te va a guiar en cada paso del método.">
+	<title>Ontarget - Quienes Somos? | OnTarget</title>
+
+	<!-- Favicons -->
+	<?php include('includes/favicon.php'); ?>
+
+	<link rel="stylesheet" type="text/css" href="./node_modules/normalize.css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="./node_modules/@fortawesome/fontawesome-free/css/all.css">
+	<link rel="stylesheet" type="text/css" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./node_modules/slick-carousel/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="./node_modules/slick-carousel/slick/slick-theme.css"/>
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+	<!-- Nav -->
+	<?php include('includes/nav.php'); ?>
+
+	<section class="quienes_somos">
+
+		<div class="bg_gris">
+
+			<!-- Header -->
+			<section class="header container-fluid text-center">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+
+							<h1>Aprende de los mejores profesionales el Método OnTarget</h1>
+							<p>
+								OnTarget es el método de aprendizaje por excelencia del Grupo WGSA, es una compañía que nace de la visión de profesionales con espíritu emprendedor y amplia trayectoria en el mercado asegurador. Aquí se fusionan la experiencia de trabajo en un marco tradicional con un nuevo enfoque, más cálido y actual, en el que la prestancia y la comunicación con el cliente marcan la diferencia.
+							</p>
+
+						</div>
+						
+					</div>
+				</div>
+			</section>
+			<!-- Header end -->
+
+			<!-- Equipo -->
+			<section class="equipo container text-center">
+				<div class="row">
+					<div class="col-md-12">
+
+						<p>
+							Nuestro "Método OnTarget" es un camino sólido para la transformación de nuevos profesionales en el mercado asegurador, que se enfoca en brindar conocimientos en un ámbito de aprendizaje y contención constante para que cada nuevo miembro de la empresa pueda desarrollar todo su potencial profesional y personal. 
+						</p>
+
+					</div>
+					
+				</div>
+			</section>
+			<!-- Equipo end -->
+
+			<!-- Equipo -->
+			<?php include('includes/equipo.php'); ?>
+			
+
+		</div>
+
+	</section>
+
+	<!-- Footer -->
+	<?php include('includes/footer.php'); ?>
+
+</body>
+<script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="./node_modules/slick-carousel/slick/slick.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+
+<script>
+	$(document).ready(function(){
+
+		$('.slick_class').slick({
+	  dots: false,
+	  appendArrows: false,
+	  autoplay: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 3,
+	        infinite: true,
+	        dots: false,
+
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 2,
+	        dots: false,
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        dots: false,
+	      }
+	    }
+	  ]
+	});
+
+	})
+</script>
+</html>
