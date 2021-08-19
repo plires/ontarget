851aff3dce029563cac0b7d63b7f30a46e826b0b
Pablo Lires <pablo@librecomunicacion.net>
2021-08-19 12:37:22 -0300

    Agregado de seccion como funciona

--- como-funciona.php
+++ como-funciona.php
@@ -21,124 +21,123 @@
 	<!-- Nav -->
 	<?php include('includes/nav.php'); ?>
 
-	<section class="como_funciona">
+	<section class="funciona">
 
 		<div class="bg_gris">
 
 			<!-- Plan de estudios -->
-			<section class="header container-fluid">
-				<div class="container">
-					<div class="row">
-						
-						<div class="col-md-12">
-							<h1>Un plan de estudios adaptado <br>a tus metas, sueños e intereses</h1>
-						</div>
-
-						<div class="col-md-6">
-							<img class="img-fluid" src="img/como-funciona/telefono.png" alt="telefono">
-						</div>
-
-						<div class="col-md-6">
-							<p>
-								El método OnTarget incluye acompañamiento constante de Team Leaders. En cada unidad  encontraran ejercicios que permiten evaluar los resultados del conocimiento y poder así obtener los mejores resultados para tus metas y necesidades. Podrás también, aprender a tu propio ritmo y personalizar tu aprendizaje.  
-							</p>
-						</div>
-
+			<section class="plan_estudios header container">
+				<div class="row">
+					
+					<div class="col-md-12">
+						<h1><span>Un plan de estudios adaptado <br>a tus metas, sueños e intereses</span></h1>
 					</div>
+
+					<div class="col-md-6">
+						<img class="img-fluid" src="img/como-funciona/telefono.png" alt="telefono">
+					</div>
+
+					<div class="col-md-6">
+						<div class="faja_sobre_titulo_izq"></div>
+						<p>
+							<span>El método OnTarget</span> incluye acompañamiento constante de Team Leaders. En cada unidad  encontraran ejercicios que permiten evaluar los resultados del conocimiento y poder así obtener los mejores resultados para tus metas y necesidades. Podrás también, aprender a tu propio ritmo y personalizar tu aprendizaje.  
+						</p>
+					</div>
+
 				</div>
 			</section>
 			<!-- Plan de estudios end -->
 
 			<!-- Como Funciona -->
-			<section class="header container-fluid">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<h2>¿Cómo Funciona?</h2>
-							<p>
-								Con solo 15 min. al día, vivirás un crecimiento personal y profesional que cambiará tu vida y tu futuro. Gozando del apoyo, la motivación y amistad de nuestro equipo. <br>
-								¿Lo mejor? Estamos juntos para planificar tu futuro.
-							</p>
-							<img class="img-fluid" src="img/como-funciona/compu.png" alt="compu">
-							<p>
-								La plataforma de aprendizaje en línea de OnTarget combina el poder del microaprendizaje diario, la tecnología, actualidad y el apoyo del equipo de WG SA para lograr una transformación personal y profesional que no podrás igualar con la formación tradicional.
-							</p>
-						</div>
+			<section class="como_funciona container">
+
+				<img class="img-fluid puntos_left" src="img/home/puntos.png" alt="puntos izquierda">
+				<img class="img-fluid puntos_right" src="img/home/puntos.png" alt="puntos derecha">
+
+				<div class="row">
+					<div class="col-md-12">
+						<h2>¿Cómo Funciona?</h2>
+						<p class="enunciado">
+							Con solo 15 min. al día, vivirás un crecimiento personal y profesional que cambiará tu vida y tu futuro. Gozando del apoyo, la motivación y amistad de nuestro equipo. <br><br>
+							¿Lo mejor? Estamos juntos para planificar tu futuro.
+						</p>
+						<img class="img-fluid" src="img/como-funciona/compu.png" alt="compu">
+						<p>
+							La plataforma de aprendizaje en línea de OnTarget combina el poder del microaprendizaje diario, la tecnología, actualidad y el apoyo del equipo de WG SA para lograr una transformación personal y profesional que no podrás igualar con la formación tradicional.
+						</p>
 					</div>
 				</div>
 			</section>
 			<!-- Como Funciona end -->
 
 			<!-- Pasos -->
-			<section class="header container-fluid">
-				<div class="container">
+			<section class="pasos container">
 
-					<div class="row">
+				<div class="row">
 
-						<div class="col-md-12">
-							<p class="paso">01</p>
-							<h3>Realiza la inscripción</h3>
-						</div>
-
-						<div class="col-md-6">
-							<img class="img-fluid" src="img/como-funciona/inscripcion.jpg" alt="inscripcion">
-						</div>
-
-						<div class="col-md-6">
-							<p>
-								El Método OnTarget es muy sensillo desde el paso 1. Solo debes ir a la sección de  "Inscripción", registrarte, luego recibirás un mail con acceso a la plataforma de aprendizaje y listo, ya estarás a bordo. Recuerda que siempre podrás aprender a tu propio ritmo y personalizar tu aprendizaje.
-							</p>
-						</div>
-
+					<div class="col-md-12 content_pasos">
+						<p class="paso_numero">01</p>
+						<h3>Realiza la inscripción</h3>
 					</div>
 
-					<div class="row">
-
-						<div class="col-md-12">
-							<p class="paso">02</p>
-							<h3>Inicia Sesión</h3>
-						</div>
-
-						<div class="col-md-6">
-							<p>
-								Una vez dentro de la Plataforma de Aprendizaje de OnTarget se te asignará un Team Leader que siempre estará disponible para responder todas tus dudas y ayudarte a avanzar durante todo el proceso.
-							</p>
-						</div>
-
-						<div class="col-md-6">
-							<img class="img-fluid" src="img/como-funciona/sesion.jpg" alt="sesion">
-						</div>
-
+					<div class="col-md-6">
+						<img class="img-fluid" src="img/como-funciona/inscripcion.jpg" alt="inscripcion">
 					</div>
 
-					<div class="row">
-
-						<div class="col-md-12">
-							<p class="paso">03</p>
-							<h3>Únete a nuestro Equipo</h3>
-						</div>
-
-						<div class="col-md-6">
-							<img class="img-fluid" src="img/como-funciona/unite.jpg" alt="unite">
-						</div>
-
-						<div class="col-md-6">
-							<p>
-								Nuestros Team Leaders te presentarán a nuestro equipo y podrás interactuar con otros estudiantes y hacer preguntas, iniciar debates, compartir publicaciones o simplemente encontrar un compañero de aprendizaje.
-							</p>
-						</div>
-
-					</div>
-
-					<div class="row">
-
-						<div class="col-md-12">
-							<button class="btn btn-primary">Hazte miembro Ahora</button>
-						</div>
-
+					<div class="col-md-6">
+						<p>
+							El Método OnTarget es muy sensillo desde el paso 1. Solo debes ir a la sección de  "Inscripción", registrarte, luego recibirás un mail con acceso a la plataforma de aprendizaje y listo, ya estarás a bordo. Recuerda que siempre podrás aprender a tu propio ritmo y personalizar tu aprendizaje.
+						</p>
 					</div>
 
 				</div>
+
+				<div class="row">
+
+					<div class="col-md-12 content_pasos cambiar_order">
+						<p class="paso_numero paso_cambiar_orden">02</p>
+						<h3>Inicia Sesión</h3>
+					</div>
+
+					<div class="col-md-6 pasos_textos">
+						<p>
+							Una vez dentro de la Plataforma de Aprendizaje de OnTarget se te asignará un Team Leader que siempre estará disponible para responder todas tus dudas y ayudarte a avanzar durante todo el proceso.
+						</p>
+					</div>
+
+					<div class="col-md-6">
+						<img class="img-fluid" src="img/como-funciona/sesion.jpg" alt="sesion">
+					</div>
+
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-12 content_pasos">
+						<p class="paso_numero">03</p>
+						<h3>Únete a nuestro Equipo</h3>
+					</div>
+
+					<div class="col-md-6">
+						<img class="img-fluid" src="img/como-funciona/unite.jpg" alt="unite">
+					</div>
+
+					<div class="col-md-6">
+						<p>
+							Nuestros Team Leaders te presentarán a nuestro equipo y podrás interactuar con otros estudiantes y hacer preguntas, iniciar debates, compartir publicaciones o simplemente encontrar un compañero de aprendizaje.
+						</p>
+					</div>
+
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-12 content_button">
+						<button class="btn btn_ontarget">Hazte miembro Ahora</button>
+					</div>
+
+				</div>
+
 			</section>
 			<!-- Pasos end -->
 
